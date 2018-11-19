1a7bfcbdf91000d6e7b251b3b51cc603880dc1a4
Foldr <rightfold+kloe@gmail.com>
2018-11-20 00:00:59 +0100

    Reorganize data items
    This includes adding more hierarchy and qualifications.

--- aodbms/src/aodbms.cbl
+++ aodbms/src/aodbms.cbl
@@ -3,18 +3,43 @@
 
        DATA DIVISION.
        WORKING-STORAGE SECTION.
+
+      ******************************************************************
+      * These data items contain ZMQ constants.
+
+       01 FILLER.
+           02 ws-zmq-rep               BINARY-LONG SIGNED VALUE 4.
+
+      ******************************************************************
+      * These data items contain the DBMS configuration.
+
        01 ws-configuration.
            02 ws-data-path             PIC X(256).
            02 ws-listen-address        PIC X(256).
 
+      ******************************************************************
+      * These data items contain ZMQ objects and ephemeral data.
+
        01 ws-zmq.
-           02 ws-zmq-rep               BINARY-LONG SIGNED VALUE 4.
-           02 ws-status                BINARY-LONG SIGNED.
-           02 ws-flags                 BINARY-LONG SIGNED.
            02 ws-context               POINTER.
            02 ws-socket                POINTER.
-           02 ws-bind-address          PIC X(257).
-           02 ws-message               PIC X(64).
+
+           02 ws-ephemeral.
+
+      *        ZMQ expects ws-message to be 8-byte aligned, and the
+      *        size must be 64 bytes to correspond to the zmq_msg_t
+      *        structure found in the zmq.h header.
+               03 ws-message               PIC X(64).
+
+      *        NUL-terminated, hence the increased size.
+               03 ws-address               PIC X(257).
+
+      *        Return status and flags are 32-bit signed integers.
+               03 ws-status                BINARY-LONG SIGNED.
+               03 ws-flags                 BINARY-LONG SIGNED.
+
+      ******************************************************************
+      * These data items used to communicate with the se-crash section.
 
        01 ws-crash.
            02 ws-origin                PIC X(20).
@@ -42,37 +67,40 @@
            .
 
        pa-initialize-zmq-context.
-           CALL STATIC 'zmq_ctx_new' GIVING ws-context
-           IF ws-context IS EQUAL TO NULL THEN
-               MOVE 'zmq_ctx_new' TO ws-origin
+           CALL STATIC 'zmq_ctx_new' GIVING ws-context OF ws-zmq
+           IF ws-context OF ws-zmq IS EQUAL TO NULL THEN
+               MOVE 'zmq_ctx_new' TO ws-origin OF ws-crash
                GO TO se-crash
            END-IF
            .
 
        pa-initialize-zmq-socket.
            CALL STATIC 'zmq_socket'
-               USING VALUE ws-context, VALUE ws-zmq-rep
-               GIVING ws-socket
-           IF ws-socket IS EQUAL TO NULL THEN
-               MOVE 'zmq_socket' TO ws-origin
+               USING VALUE ws-context OF ws-zmq, VALUE ws-zmq-rep
+               GIVING ws-socket OF ws-zmq
+           IF ws-socket OF ws-zmq IS EQUAL TO NULL THEN
+               MOVE 'zmq_socket' TO ws-origin OF ws-crash
                GO TO se-crash
            END-IF
            .
 
        pa-initialize-zmq-bind.
            STRING ws-listen-address DELIMITED BY SPACES, X'00'
-               INTO ws-bind-address
+               INTO ws-address OF ws-zmq
            CALL STATIC 'zmq_bind'
-               USING VALUE ws-socket, REFERENCE ws-bind-address
-               GIVING ws-status
-           IF ws-status IS EQUAL TO -1 THEN
-               MOVE 'zmq_bind' TO ws-origin
+               USING VALUE ws-socket OF ws-zmq
+                     REFERENCE ws-address OF ws-zmq
+               GIVING ws-status OF ws-zmq
+           IF ws-status OF ws-zmq IS EQUAL TO -1 THEN
+               MOVE 'zmq_bind' TO ws-origin OF ws-crash
                GO TO se-crash
            END-IF
            .
 
        pa-initialize-zmq-msg.
-           CALL STATIC 'zmq_msg_init' USING ws-message GIVING ws-status
+           CALL STATIC 'zmq_msg_init'
+               USING ws-message OF ws-zmq
+               GIVING ws-status OF ws-zmq
            .
 
       ******************************************************************
@@ -81,31 +109,31 @@
        se-command SECTION.
 
        pa-command-receive.
-           MOVE 0 TO ws-flags
+           MOVE 0 TO ws-flags OF ws-zmq
            CALL STATIC 'zmq_msg_recv'
-               USING REFERENCE ws-message
-                     VALUE ws-socket
-                     VALUE ws-flags
-               GIVING ws-status
-           IF ws-status IS EQUAL TO -1 THEN
-               MOVE 'zmq_msg_recv' TO ws-origin
+               USING REFERENCE ws-message OF ws-zmq
+                     VALUE ws-socket OF ws-zmq
+                     VALUE ws-flags OF ws-zmq
+               GIVING ws-status OF ws-zmq
+           IF ws-status OF ws-zmq IS EQUAL TO -1 THEN
+               MOVE 'zmq_msg_recv' TO ws-origin OF ws-crash
                GO TO se-crash
            END-IF
            .
 
        pa-command-process.
-           DISPLAY ws-message
+           DISPLAY ws-message OF ws-zmq
            .
 
        pa-command-respond.
-           MOVE 0 TO ws-flags
+           MOVE 0 TO ws-flags OF ws-zmq
            CALL STATIC 'zmq_msg_send'
-               USING REFERENCE ws-message
-                     VALUE ws-socket
-                     VALUE ws-flags
-               GIVING ws-status
-           IF ws-status IS EQUAL TO -1 THEN
-               MOVE 'zmq_msg_send' TO ws-origin
+               USING REFERENCE ws-message OF ws-zmq
+                     VALUE ws-socket OF ws-zmq
+                     VALUE ws-flags OF ws-zmq
+               GIVING ws-status OF ws-zmq
+           IF ws-status OF ws-zmq IS EQUAL TO -1 THEN
+               MOVE 'zmq_msg_send' TO ws-origin OF ws-crash
                GO TO se-crash
            END-IF
            .
@@ -116,7 +144,7 @@
        se-crash SECTION.
 
        pa-crash-announce.
-           DISPLAY FUNCTION TRIM(ws-origin) UPON SYSERR
+           DISPLAY FUNCTION TRIM(ws-origin OF ws-crash) UPON SYSERR
            .
 
        pa-crash-exit.
